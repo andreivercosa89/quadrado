b20dabe4f21408774428995ee8050a56f675309c
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-25 21:04:44 -0400

    quadrado em objeto

---
+++ quadrado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado = new Quadrado();
+            Console.WriteLine("Informe o Lado A: ");
+
+            quadrado.defineValorLadoA(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe o Lado B: ");
+
+            quadrado.defineValorLadoB(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe o Lado C: ");
+
+            quadrado.defineValorLadoC(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Informe o Lado D: ");
+
+            quadrado.defineValorLadoD(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Lado A: " + quadrado.getvalorLadoA());
+            Console.WriteLine("Lado B: " + quadrado.getvalorLadoB());
+            Console.WriteLine("Lado C: " + quadrado.getvalorLadoC());
+            Console.WriteLine("Lado D: " + quadrado.getvalorLadoD());
+        }
+    }
+}
---
+++ quadrado/Quadrado.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace quadrado
+{
+    public class Quadrado
+    {
+        private int LadoA;
+        private int LadoB;
+        private int LadoC;
+        private int LadoD;
+
+        public void defineValorLadoA( int valorLadoA){
+            LadoA = valorLadoA;
+        }
+
+        public int getvalorLadoA (){
+            return LadoA;
+        }
+
+        public void defineValorLadoB(int valorLadoB)
+        {
+            LadoB = valorLadoB;
+        }
+
+        public int getvalorLadoB()
+        {
+            return LadoB;
+        }
+
+        public void defineValorLadoC(int valorLadoC)
+        {
+            LadoC = valorLadoC;
+        }
+
+        public int getvalorLadoC()
+        {
+            return LadoC;
+        }
+
+        public void defineValorLadoD(int valorLadoD)
+        {
+            LadoD = valorLadoD;
+        }
+
+        public int getvalorLadoD()
+        {
+            return LadoD;
+        }
+
+        public Quadrado()
+        {
+        }
+    }
+}
